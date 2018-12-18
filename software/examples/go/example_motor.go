aa439ad969b4d3b0dbc91a7ab76a9a0026e51898
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 15:21:50 +0100

    go: Change import path again.

--- software/examples/go/example_motor.go
+++ software/examples/go/example_motor.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/tinkerforge/go-api-bindings/ipconnection"
-	"github.com/tinkerforge/go-api-bindings/motorized_linear_poti_bricklet"
+	"github.com/Tinkerforge/go-api-bindings/ipconnection"
+	"github.com/Tinkerforge/go-api-bindings/motorized_linear_poti_bricklet"
 )
 
 const ADDR string = "localhost:4223"
