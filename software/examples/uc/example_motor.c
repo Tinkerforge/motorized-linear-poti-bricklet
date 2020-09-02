bc9a793027720187ce181624f0224a7ee775a842
Erik Fleckstein <erik@tinkerforge.com>
2020-09-02 09:56:18 +0200

    uc: Add example driver note.

--- software/examples/uc/example_motor.c
+++ software/examples/uc/example_motor.c
@@ -1,3 +1,7 @@
+// This example is not self-contained.
+// It requres usage of the example driver specific to your platform.
+// See the HAL documentation.
+
 #include "bindings/hal_common.h"
 #include "bindings/bricklet_motorized_linear_poti.h"
 
